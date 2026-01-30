2c9067da0c83b606c502e9e13041ddf39da4f751
JessDaodao <694428809@qq.com>
2026-01-30 14:59:06 +0800

    小调整

--- src/main/java/fun/eqad/ponyparkour/listener/ParkourListener.java
+++ src/main/java/fun/eqad/ponyparkour/listener/ParkourListener.java
@@ -13,6 +13,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -179,6 +180,17 @@ public class ParkourListener implements Listener {
 
     @EventHandler
     public void onEntityDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            if (parkourManager.isPlaying(player)) {
+                event.setCancelled(true);
+                player.setFireTicks(0);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityCombust(EntityCombustEvent event) {
         if (event.getEntity() instanceof Player) {
             Player player = (Player) event.getEntity();
             if (parkourManager.isPlaying(player)) {
